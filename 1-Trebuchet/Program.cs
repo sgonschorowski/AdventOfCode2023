6e9f5cbf7e05e776a09f2efbb23bd035ca61125d
Soenke Gonschorowski <soenke.gonschorowski@sennheiser.com>
2023-12-03 11:09:29 +0100

    added part 2 of day 1

--- 1-Trebuchet/Program.cs
+++ 1-Trebuchet/Program.cs
@@ -1,10 +1,46 @@
-﻿using System.Numerics;
-
-var file = "Assets/1-Trebuchet-Input.txt";
-var lines = File.ReadAllLines(file);
+﻿var file = "Assets/1-Trebuchet-Input.txt";
+List<string> linesFromFile = File.ReadAllLines(file).ToList();
 var sum = 0;
+Dictionary<string, int> numbers = new Dictionary<string, int>()
+{
+    { "twone", 21 },
+    { "threeigth", 38 },
+    { "fiveigth", 58 },
+    { "eightwo", 82 },
+    { "nineigth", 98 },
+    { "sevenine", 79 },
+    { "oneight", 18 },
+    { "one", 1 },
+    { "two", 2 },
+    { "three", 3 },
+    { "four", 4 },
+    { "five", 5 },
+    { "six", 6 },
+    { "seven", 7 },
+    { "eight", 8 },
+    { "nine", 9 }
+};
+var preparedLines = new List<string>();
+var concatedNumbers = new List<string>();
 
-foreach (var line in lines)
+foreach (var lineFromFile in linesFromFile)
+{
+    var findings = lineFromFile;
+    foreach (var item in numbers)
+    {
+        if (findings.Contains(item.Key))
+        {
+
+            findings = findings.Replace(item.Key, item.Value.ToString());
+        }
+    }
+    Console.WriteLine("Prepared line: {0}", findings);
+    preparedLines.Add(findings);
+
+}
+
+
+foreach (var line in preparedLines)
 {
     char[] chars = [.. line];
     var firstDigit = 0;
@@ -32,14 +68,18 @@ foreach (var line in lines)
     }
 
     if (lastDigit is -1)
+    {
         lastDigit = firstDigit;
+    }
 
     var concatedNumber = String.Concat(firstDigit, lastDigit);
+    concatedNumbers.Add(concatedNumber);
 
-    Console.WriteLine("Concatenated number: {0}", concatedNumber);
-
-    _ = int.TryParse(concatedNumber, out var numberToAdd);
-
+    var b = int.TryParse(concatedNumber, out var numberToAdd);
+    if (!b)
+    {
+        continue;
+    }
     sum += numberToAdd;
 }
 
